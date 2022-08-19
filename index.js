f011715c99d461059573f1fda6b2d402d577efa1
alepaez-dev <alepaezdev@gmail.com>
2022-08-18 21:25:07 -0500

    feat: create Koder

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ require('dotenv').config()
 const mongoose = require("mongoose")
 const express = require("express")
 
+/*
+* Modelos -> Collecion
+* Schemas -> documento de esa collection
+*/
+
+// App
 const app = express()
 
-// AWS -> keys privadas, keys accesos
+// Middlewares
+app.use(express.json())
+
 
 // Destructuracion
 // Variables de entorno
@@ -15,8 +23,66 @@ const {
   DB_NAME
 }  = process.env
 
+// Schemas de mongo
+const koderSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    maxlength: 20,
+    required: true
+  },
+  modulo: {
+    type: String
+  },
+  edad: {
+    type: Number,
+    min: 18,
+    max: 150
+  },
+  generacion: {
+    type: String,
+    required: true
+  },
+  sexo: {
+    type: String,
+    enum: ["f", "m", "o"]
+  }
+})
+
+// Modelos -> Collection
+const Koder = mongoose.model("koders", koderSchema)
+
 // URL
 const URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}${DB_NAME}`
+
+// Endpoints
+app.post("/koders", async (request, response) => {
+  // Destructurandp
+  const { body } = request
+
+  console.log("body", body)
+  try {
+    // Accedemos a la bd -> Promesa
+    const koder = await Koder.create(body)
+    console.log("koder", koder)
+    response.status(201)
+    response.json({
+      success: true,
+      data: {
+        koder
+      }
+    })
+
+  }catch(error) {
+    console.log("error", error)
+    response.status(400)
+    response.json({
+      success: false,
+      message: error.message
+    })
+  }
+})
+
 
 // Conectando a la base de datos
 mongoose.connect(URL)
